4d9f8f62c29a66c62e17394add937594dd95982e
peter_heard01 <peter_heard01@hotmail.com>
2015-03-05 01:24:16 +0000

    Refactored loader and added mapping in

--- EventLst.Core/EventsLoader.cs
+++ EventLst.Core/EventsLoader.cs
@@ -11,33 +11,47 @@ namespace EventLst.Core
         public string Lat { get; set; }
 
         private IEventProvider _provider;
+
         public EventsLoader(IEventProvider injectedProvider)
         {
             _provider = injectedProvider;
-        }
-
-        public DateTime FromUnixTime(long unixTime)
-        {
-            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-            return epoch.AddSeconds(unixTime);
         }
 
         public List<EventModel> Load()
         {
             if (Lon == null || Lat == null) throw new Exception("You are missing longitude or latitude param");
 
+            var jsonDtoResult = LoadJson();
+
+            return MapDtoObjectsToModels(jsonDtoResult);
+        }
+
+        private List<EventModel> MapDtoObjectsToModels(dynamic jsonDtoResult)
+        {
             var retModels = new List<EventModel>();
-
-            var jsonAsString = _provider.Load(Lon,Lat);
-
-            dynamic jsonAsDynamicObject = JObject.Parse(jsonAsString);
-
-            foreach (var dto in jsonAsDynamicObject.results)
+            foreach (var dto in jsonDtoResult.results)
             {
-                retModels.Add(new EventModel());
+                retModels.Add(new EventModel()
+                {
+                    Name = dto.name,
+                    DateAndTime = FromUnixTime(Convert.ToInt64(dto.time)),
+                    City = dto.venue.city,
+                    HtmlDescription = dto.description
+                });
             }
-
             return retModels;
+        }
+
+        private dynamic LoadJson()
+        {
+            var jsonAsString = _provider.Load(Lon, Lat);
+            return JObject.Parse(jsonAsString);
+        }
+
+        private DateTime FromUnixTime(long unixTimeMilliSeconds)
+        {
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return epoch.AddSeconds(unixTimeMilliSeconds / 1000);
         }
     }
 }
