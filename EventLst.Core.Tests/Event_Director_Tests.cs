09a6bde622abc4bb16b196c90a547b9555af764f
peter_heard01 <peter_heard01@hotmail.com>
2015-03-05 19:06:18 +0000

    added test data builder

--- EventLst.Core.Tests/Event_Director_Tests.cs
+++ EventLst.Core.Tests/Event_Director_Tests.cs
@@ -4,52 +4,34 @@ using Moq;
 
 namespace EventLst.Core.Tests
 {
-
-    //public class DirectorBuilder
-    //{
-    //    private Mock<IEventProvider> provider;
-    //    private ResultsDirector eventsDirector;
-    //    private EventBuilder eventBuilder;
-    //}
-
     [TestFixture]
     public class EventLoaderTests
     {
-        private Mock<IEventProvider> provider;
-        private ResultsDirector eventsDirector;
-        private EventBuilder eventBuilder;
-
-        [SetUp]
-        public void SetUp()
-        {
-            provider = new Mock<IEventProvider>();
-            eventBuilder = new EventBuilder(provider.Object,new EventsDtoMapper());
-            eventsDirector = new ResultsDirector(eventBuilder);
-            provider.Setup(pro => pro.Load(It.IsAny<string>(), It.IsAny<string>())).Returns(@"{'results':[]}");
-        }
-
         [Test]
         public void Will_Throw_Exception_If_Lon_Missing()
         {
-            eventBuilder.InputModel.Lat = "1.1";
+            var eventDirector = new DirectorBuilder().WithLongitudeAndLatitude(null, "1.1").Build();
 
-            Assert.Throws<Exception>(() => eventsDirector.Construct(), "You are missing longitude or latitude param");
+            Assert.Throws<Exception>(() => eventDirector.Construct(), "You are missing longitude or latitude param");
         }
 
         [Test]
         public void Will_Throw_Exception_If_Lat_Missing()
         {
-            eventBuilder.InputModel.Lon = "1.1";
+            var eventDirector = new DirectorBuilder().WithLongitudeAndLatitude("1.1", null).Build();
 
-            Assert.Throws<Exception>(() => eventsDirector.Construct(), "You are missing longitude or latitude param");
+            Assert.Throws<Exception>(() => eventDirector.Construct(), "You are missing longitude or latitude param");
         }
 
         [Test]
         public void Will_Pass_Longitude_And_Latitude_To_Provider()
         {
-            PopulateCoOrds();
+            var provider = new Mock<IEventProvider>();
+            provider.Setup(pro => pro.Load(It.IsAny<string>(), It.IsAny<string>())).Returns(@"{'results':[]}");
 
-            eventsDirector.Construct();
+            var eventDirector = new DirectorBuilder().WithProvider(provider.Object).WithLongitudeAndLatitude("1.1", "1.2").Build();
+
+            eventDirector.Construct();
 
             provider.Verify(spy => spy.Load("1.1", "1.2"), Times.Exactly(1));
         }
@@ -57,40 +39,29 @@ namespace EventLst.Core.Tests
         [Test]
         public void Will_Load_4_Models_When_4_Results()
         {
-            IEventProvider meetupStubbedProvider = new DiskEventProvider("/Doubles/meetup_open_events_response_stub.json");
-            eventBuilder = new EventBuilder(meetupStubbedProvider, new EventsDtoMapper());
-            eventsDirector = new ResultsDirector(eventBuilder);
+            var builder = new DirectorBuilder();
+            var eventDirector = builder.WithLongitudeAndLatitude("1.1", "1.2").Build();
 
-            PopulateCoOrds();
+            eventDirector.Construct();
 
-            eventsDirector.Construct();
-
-            Assert.AreEqual(4, eventBuilder.OutputModel.Count);
+            Assert.AreEqual(4, builder.eventBuilder.OutputModel.Count);
         }
 
         [Test]
         public void Will_Map_Dto_Properties_To_Model_Properties()
         {
-            IEventProvider meetupStubbedProvider = new DiskEventProvider("/Doubles/meetup_open_events_response_stub.json");
-            eventBuilder = new EventBuilder(meetupStubbedProvider, new EventsDtoMapper());
-            eventsDirector = new ResultsDirector(eventBuilder);
+            var builder = new DirectorBuilder();
+            var eventDirector = builder.WithLongitudeAndLatitude("1.1", "1.2").Build();
 
-            PopulateCoOrds();
+            eventDirector.Construct();
 
-            eventsDirector.Construct();
-
-            var firstEvent = eventBuilder.OutputModel[0];
+            var firstEvent = builder.eventBuilder.OutputModel[0];
 
             Assert.AreEqual(firstEvent.Name, "The Oxford Spanish Language Exchange Meetup");
-            Assert.AreEqual(firstEvent.DateAndTime, new DateTime(2015,03,04,18,30,00));
+            Assert.AreEqual(firstEvent.DateAndTime, new DateTime(2015, 03, 04, 18, 30, 00));
             Assert.AreEqual(firstEvent.City, "Oxford");
             Assert.AreEqual(firstEvent.HtmlDescription, "<h1>I am the description</h1>");
         }
 
-        private void PopulateCoOrds()
-        {
-            eventBuilder.InputModel.Lon = "1.1";
-            eventBuilder.InputModel.Lat = "1.2";
-        }
     }
 }
